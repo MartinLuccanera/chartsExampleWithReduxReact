0c63cc6664148cc35d1a3c4c61195d49fc7b5263
Martin Luccanera <martin.luccanera@gmail.com>
2018-11-05 14:00:12 -0300

    Code added in Section 5 Lecture 67.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,14 +8,16 @@ class WeatherList extends Component {
         const name = cityData.city.name;
         //temps will be an array of all the temps that come with cityData.
         const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         console.log(temps);
         return (
           <tr key={name}>
               <td>{name}</td>
-              <td>
-                  <Chart data={temps} color="orange" />
-              </td>
+              <td><Chart data={temps} unit="ºk" color="orange" /></td>
+              <td><Chart data={pressures} unit="hPa" color="green" /></td>
+              <td><Chart data={humidities} unit="%" color="black" /></td>
           </tr>
         );
     }
