77b6e44f24e3a5c21647ce61552a9661c371e811
Martin Luccanera <martin.luccanera@gmail.com>
2018-11-05 12:28:42 -0300

    Code added in Section 5 Lecture 63.

---
+++ src/containers/weather_list.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    }
+}
+
+/**
+ * We are using state.weather because we assigned the WeatherReducer to a weather key in reducers.index (combinedReducers)
+ *
+ * @param state
+ * @returns {{weather: *|Function}}
+ */
+function mapStateToProps({ weather }) {
+    //placing { weather } instead of state in 'mapStateToProps({ weather })' is the same as receiving weather as parameter
+    // and doing 'const weather = state.weather'. And if we have weather: weather as key-value pair we can do ->
+    return { weather };
+}
+
+//We are exporting the connected version of WeatherList
+export default connect(mapStateToProps)(WeatherList);
