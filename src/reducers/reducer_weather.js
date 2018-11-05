e8dbdfef3f958803990ee9f98f5a8a91e415b1b3
Martin Luccanera <martin.luccanera@gmail.com>
2018-11-05 11:53:31 -0300

    Code added in Section 5 Lecture 62.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,6 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
     /* When reaching this part of the code, the request will no longer be a promise but the result of the AJAX request
      * to the API. It will contain the data that was returned by the server.
      * Remember that middlewares (like redux promise) have the ability to stop or manipulate actions before they hit
@@ -7,5 +9,14 @@ export default function(state = null, action) {
      * a new action of the same type but with a payload of a resolved request (sends the result instead of the promise,
      * which in this case is an asynchronous call).
      */
+
+    switch (action.type) {
+        case FETCH_WEATHER:
+            //We need to return the new state with the sum of all the city the user has searched for + the new search.
+            //return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state ];//ES6 version of the former line. Returns an array concatenated
+                                                     //with the new element. DO NOT MUTATE STATE neither by 
+                                                     //(state = new state) nor by state.push
+    }
     return state;
 }
